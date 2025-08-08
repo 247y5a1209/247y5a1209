5777b5ddf6f64d9d5176f5ca417a843e6ec0d99c
247y5a1209 <247y5a1209@mlritm.ac.in>
2025-08-07 22:31:29 -0700

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,8 @@
+# Get input from the user
+name = input("Please enter your name: ")
+
+# Process the input and create a greeting message
+greeting = "Hello, " + name + "! Welcome to a simple Python program."
+
+# Print the greeting message
+print(greeting)
